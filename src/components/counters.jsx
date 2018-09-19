98bf38527c6417fe9de71fb6908c5abf890bdc27
Amjad Khan <amkh@microsoft.com>
2018-09-19 10:22:32 +0530

    Added Navbar implementation

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,42 +2,18 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 0 },
-      { id: 2, value: 2 },
-      { id: 3, value: 0 },
-      { id: 4, value: 7 },
-      { id: 5, value: 0 }
-    ]
-  };
-  handleDelete = counterId => {
-    const counters = this.state.counters.filter(c => c.id !== counterId);
-    this.setState({ counters });
-  };
-  handleIncrement = counter => {
-    counter.value = counter.value + 1;
-    this.setState({ counter });
-  };
-  handleReset = () => {
-    const counters = this.state.counters.map(c => {
-      c.value = 0;
-      return c;
-    });
-    this.setState({ counters });
-  };
   render() {
     return (
       <div>
-        <button onClick={this.handleReset} className="btn-primary btn-sm m-2">
+        <button onClick={this.props.onReset} className="btn-primary btn-sm m-2">
           Reset
         </button>
-        {this.state.counters.map(counter => (
+        {this.props.counters.map(counter => (
           <Counter
             key={counter.id}
             value={counter.value}
-            onDelete={this.handleDelete}
-            onIncrement={this.handleIncrement}
+            onDelete={this.props.onDelete}
+            onIncrement={this.props.onIncrement}
             counter={counter}
           />
         ))}
